5026999765bca8a6be3fa5b3f789fafaf64d5841
20ashisharma <72648498+20ashisharma@users.noreply.github.com>
2023-12-04 12:46:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "testinginfracost"
+  }
+}
