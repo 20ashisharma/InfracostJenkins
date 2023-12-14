f555b0af267c217f9ce82f89e8990d47f8037ee3
20ashisharma <72648498+20ashisharma@users.noreply.github.com>
2023-12-14 16:14:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  
+}
 
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "testinginfracost"
+  }
+}
+resource "aws_instance" "database_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t3a.medium"
+
+  tags = {
+    Name = "testinginfracost"
+  }
+}
